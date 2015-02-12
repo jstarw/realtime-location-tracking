464ca7990fece2112d510940793fc58d559b7b39
unknown <awarkentin@awarkentin-2.klick.com>
2015-02-12 14:55:35 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+
+# Socket.IO Chat
+
+A simple chat demo for socket.io
+
+## How to use
+
+```
+$ cd realtime-location-tracking
+$ npm install
+$ node index.js
+```
+
+And point your browser to `http://localhost:3000/master` for the master page,
+and `http://localhost:3000/mobile` for the mobile page.
+
+## Features
+The app will track the lcoations in real time of multiple users.
---
+++ index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/mobile', function(req, res){
+  res.sendfile('public/mobile/index.html');
+});
+
+app.get('/master', function(req, res){
+  res.sendfile('public/master/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
