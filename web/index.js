123848899a5a22cc0fb140c3faec0c75aac95cfb
Jonathan Wen <jonathanwen95@gmail.com>
2015-02-25 21:57:06 +0000

    Added Map Functionality

--- web/index.js
+++ web/index.js
@@ -3,19 +3,31 @@ var app = express();
 var path = require('path');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+function randomNumber() {
+  return Math.floor(Math.random()*360)-180;
+}
 
 app.use(express.static(path.join(__dirname, 'master')));
 
 app.get('/', function(req, res){
   res.sendfile('index.html');
 });
-
 io.on('connection', function(socket){
 	console.log("a client Connected");
-	socket.emit('message', 'If you see this message, then socket.io is working correctly');
+// 	socket.emit('test', 'If you see this message, then socket.io is working correctly');
+  setInterval(function(){
+    var loc={
+      username:'Jonathan',
+      latitude: randomNumber(),
+      longitude: randomNumber()
+    };
+    socket.emit('test', loc);
+  }, 3000);
+    
+  
   socket.on('loc', function(loc){
     console.log('location: ' + loc);
-    socket.broadcast.emit('loc', loc);
+    socket.broadcast.emit('location', loc);
   });
   socket.on('disconnect', function(){
     console.log('a client disconnected');
--- web/master/main.js
+++ web/master/main.js
@@ -1,7 +1,55 @@
+var socket = io();
+
+//     socket.on('location', function(loc){
+// //       console.log("Got Location");
+// //       $('#location').append($('<li>').text('username: ' + loc.username + ', latitude: ' + loc.latitude + ', longitude: ' + loc.longitude));
+//     });
+    
 $(document).ready(function(){
     $(".menu-button").click(function() {
-      //$("#menu").toggle();
       $("#menu").animate({width:'toggle'},225);
     });
+  
 });
+
+function setUser(loc) {
+  $('#menu').append('');
+}
+
+function initialize() {
+  var mapOptions = {
+    center: { lat: -34.397, lng: 150.644},
+    zoom: 4
+  };
+  var map = new google.maps.Map(document.getElementById('map'),
+      mapOptions);
+//     google.maps.event.addDomListener(window, 'click', function () {
+//       map.setCenter({lat:-34.000, lng: 150.000});
+//     });
+  var marker = new google.maps.Marker({
+    position: map.getCenter(),
+    map: map,
+    title: 'Click to zoom'
+  });
+
+  google.maps.event.addListener(map, 'center_changed', function() {
+    // 3 seconds after the center of the map has changed, pan back to the
+    // marker.
+    window.setTimeout(function() {
+      map.panTo(marker.getPosition());
+    }, 3000);
+  });
+  //sets the center of the map to the user's location
+  socket.on('test', function(loc) {
+      console.log(loc);
+      document.getElementById('user-1').innerHTML = loc.username;
+      var latlng = {lat:loc.latitude, lng:loc.longitude};
+      map.setCenter(latlng);
+      marker.setPosition(latlng);
+    });
+}
+
+
+
+google.maps.event.addDomListener(window, 'load', initialize);
 
